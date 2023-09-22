fb60f5f774cb5be45b8ac071b66cf0f4875b62c6
TagRomEdu <qwertystyles@gmail.com>
2023-09-22 16:07:05 +0500

    Added model display settings

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Category, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+    list_filter = ('name',)
+    list_search = ('name',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'category',)
+    list_filter = ('category',)
+    list_search = ('name', 'description')
