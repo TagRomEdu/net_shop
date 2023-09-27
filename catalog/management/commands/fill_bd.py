31d0aaa617f6a5dc898161932f243e48767f4ffb
TagRomEdu <qwertystyles@gmail.com>
2023-09-27 14:23:08 +0500

    added creating products with fixtures

--- catalog/management/commands/fill_bd.py
+++ catalog/management/commands/fill_bd.py
@@ -2,19 +2,27 @@ import json
 
 from django.core.management import BaseCommand
 
-from catalog.models import Category
+from catalog.models import Category, Product
 
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
 
         Category.objects.all().delete()
+        Category.objects.all().delete()
 
         with open('data.json') as file:
             data_to_create = json.load(file)
 
         cat_to_create = []
-        for data in data_to_create:
-            cat_to_create.append(Category(**data['fields']))
+
+        for i, data in enumerate(data_to_create[:5]):
+            cat_to_create.append(Category(**data['fields'], pk=i+1))
 
         Category.objects.bulk_create(cat_to_create)
+
+        for i, data in enumerate(data_to_create[5:]):
+            Product.objects.create(pk=i+1, name=data['fields']['name'],
+                                   description=data['fields']['description'],
+                                   category=Category.objects.get(pk=data['fields']['category'] % 10),
+                                   price=data['fields']['price'])
