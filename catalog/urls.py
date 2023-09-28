2337aca641c3f2bbf7178126412062ba1b610365
TagRomEdu <qwertystyles@gmail.com>
2023-09-28 14:40:52 +0500

    cosmetic fixes

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
 
+from catalog.apps import CatalogConfig
 from catalog.views import home, contacts, product
 
+app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', home),
