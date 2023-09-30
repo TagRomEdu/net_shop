0720d47e7aaccf09ccbaa3c258fd09edb1c7c816
TagRomEdu <qwertystyles@gmail.com>
2023-09-30 15:51:24 +0500

    added Blog to admin.py

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from catalog.models import Category, Product
+from catalog.models import Category, Product, Blog
 
 
 @admin.register(Category)
@@ -14,3 +14,8 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'price', 'category', 'creating_date', 'modified_date')
     list_filter = ('category',)
     list_search = ('name', 'description')
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'slug', 'created_at', 'is_published', 'view_count')
+    list_search = ('name', 'text')
