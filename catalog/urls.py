b1f3d271b5a572bd17f9398a6ec3568c8df5f49f
TagRomEdu <qwertystyles@gmail.com>
2023-09-30 13:13:05 +0500

    reworked path for ProductDetailView

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 
 
 from catalog.apps import CatalogConfig
-from catalog.views import contacts, product, ProductListView
+from catalog.views import contacts, ProductListView, ProductDetailView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', ProductListView.as_view()),
     path('contacts/', contacts),
-    path('product/<int:id>/', product)
+    path('product/<int:pk>/', ProductDetailView.as_view())
 ]
