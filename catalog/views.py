8bf5d76bebf476ad94d2c9c116ce68497b68097c
TagRomEdu <qwertystyles@gmail.com>
2023-10-01 10:30:10 +0500

    added dynamic slug

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from pytils.translit import slugify
 
 from catalog.models import Product, Blog
 
@@ -21,6 +22,13 @@ class BlogCreateView(CreateView):
     model = Blog
     fields = ('name', 'preview', 'text', 'is_published')
     success_url = reverse_lazy('catalog:list')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_blog = form.save()
+            new_blog.slug = slugify(new_blog.name)
+            new_blog.save()
+        return super().form_valid(form)
 
 
 class BlogListView(ListView):
