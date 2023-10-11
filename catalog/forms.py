e93eb3f9980cab032e683a5e071a5ca2159ae622
TagRomEdu <qwertystyles@gmail.com>
2023-10-11 13:45:33 +0500

    added validation for name and discription

--- catalog/forms.py
+++ catalog/forms.py
@@ -21,6 +21,29 @@ class ProductForm(forms.ModelForm):
         model = Product
         fields = ('category', 'name', 'description', 'photo', 'price')
 
+    def clean_name(self):
+        worst_words = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']
+        cleaned_data: str = self.cleaned_data.get('name')
+
+        for word in worst_words:
+            if word in cleaned_data.lower():
+                raise forms.ValidationError ("В имени продукта гадкое слово, придумайте что-то получше!")
+
+        return cleaned_data
+
+    @property
+    def clean_description(self):
+        worst_words = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']
+        cleaned_data: str = self.cleaned_data.get('description')
+
+        for word in worst_words:
+            if word in cleaned_data.lower():
+                raise (forms.ValidationError
+                       (f"В описании жуткое написано! Проверьте, нет ли там следующих слов: {', '.join(worst_words)}"))
+
+        return cleaned_data
+
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
