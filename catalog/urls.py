ecdd52ce8e47873eae850c7170a7564aad92858d
TagRomEdu <qwertystyles@gmail.com>
2023-10-11 09:56:46 +0500

    added update and create urls for products

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,14 +3,17 @@ from django.urls import path
 
 from catalog.apps import CatalogConfig
 from catalog.views import (contacts, ProductListView, ProductDetailView,
-                           BlogListView, BlogDetailView, BlogCreateView, BlogDeleteView, BlogUpdateView)
+                           BlogListView, BlogDetailView, BlogCreateView, BlogDeleteView, BlogUpdateView,
+                           ProductCreateView, ProductUpdateView)
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', ProductListView.as_view()),
+    path('', ProductListView.as_view(), name='main'),
     path('contacts/', contacts),
-    path('product/<int:pk>/', ProductDetailView.as_view()),
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='product'),
+    path('prod_create/', ProductCreateView.as_view(), name='prod_create'),
+    path('product/<int:pk>/update/', ProductUpdateView.as_view(), name='prod_update'),
     path('blog_list/', BlogListView.as_view(), name='list'),
     path('blog_list/form/', BlogCreateView.as_view(), name='create'),
     path('blog_list/update/<slug>/', BlogUpdateView.as_view(), name='update'),
