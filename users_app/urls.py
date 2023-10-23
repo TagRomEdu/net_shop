a85fc0e1cdd7342d6002258edf6861312d22b6ed
TagRomEdu <qwertystyles@gmail.com>
2023-10-23 10:39:22 +0500

    added url for registration

--- users_app/urls.py
+++ users_app/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from users_app.apps import UsersAppConfig
-from users_app.views import LoginView, LogoutView
+from users_app.views import LoginView, LogoutView, RegisterView
 
 app_name = UsersAppConfig.name
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout')
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', RegisterView.as_view(), name='register')
 ]
