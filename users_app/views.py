5e1c05a4c6ff77a3f81d8fa6ec4226c86f8630d8
TagRomEdu <qwertystyles@gmail.com>
2023-10-23 10:39:34 +0500

    added controller for creating users

--- users_app/views.py
+++ users_app/views.py
@@ -1,5 +1,10 @@
 from django.contrib.auth.views import LoginView as BaseLoginView
 from django.contrib.auth.views import LogoutView as BaseLogoutView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from users_app.forms import UserForm
+from users_app.models import User
 
 
 class LoginView(BaseLoginView):
@@ -8,3 +13,10 @@ class LoginView(BaseLoginView):
 
 class LogoutView(BaseLogoutView):
     pass
+
+
+class RegisterView(CreateView):
+    model = User
+    form_class = UserForm
+    success_url = reverse_lazy('catalog:main')
+    template_name = 'users_app/register.html'
