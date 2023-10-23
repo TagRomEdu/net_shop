e1b0c6c30deec37db58b5c5d97c465b57e0be084
TagRomEdu <qwertystyles@gmail.com>
2023-10-23 10:52:27 +0500

    added form for authentication

--- users_app/forms.py
+++ users_app/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 from users_app.models import User
 
@@ -7,3 +7,21 @@ class UserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('email', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+
+class CustomAuthForm(AuthenticationForm):
+    class Meta:
+        model = User
+        fields = ('email', 'password1')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
--- users_app/views.py
+++ users_app/views.py
@@ -3,11 +3,13 @@ from django.contrib.auth.views import LogoutView as BaseLogoutView
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
-from users_app.forms import UserForm
+from users_app.forms import UserForm, CustomAuthForm
 from users_app.models import User
 
 
 class LoginView(BaseLoginView):
+    model = User
+    form_class = CustomAuthForm
     template_name = 'users_app/login.html'
 
 
