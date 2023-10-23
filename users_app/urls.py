05a900fc3ced6de2ec64c478d7ccebfc872ffe92
TagRomEdu <qwertystyles@gmail.com>
2023-10-23 09:46:10 +0500

    added urls for login and logout

--- users_app/urls.py
+++ users_app/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 
 from users_app.apps import UsersAppConfig
+from users_app.views import LoginView, LogoutView
 
 app_name = UsersAppConfig.name
+
+urlpatterns = [
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout')
+]
