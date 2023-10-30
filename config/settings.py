3b9bd78f4f966010d8fbb014df940592bfb6ca84
TagRomEdu <qwertystyles@gmail.com>
2023-10-30 12:37:49 +0500

    added dotenv params to settings

--- config/settings.py
+++ config/settings.py
@@ -10,11 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
+from dotenv import load_dotenv
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(BASE_DIR/'.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -146,11 +149,10 @@ EMAIL_HOST_USER = 'm@sster.ru'
 EMAIL_HOST_PASSWORD = os.getenv('YA_PASS')
 EMAIL_USE_SSL = True
 
-CACHE_ENABLED = True
-
+CACHE_ENABLED = os.getenv('CACHE_ENABLED') == 'True'
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.redis.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379',
+        'LOCATION': os.getenv('CACHE_LOCATED'),
     }
 }
